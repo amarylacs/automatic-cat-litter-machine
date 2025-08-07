2595989e3f35a3c27263fc4613fe804cc5448e9a
chenliziyan <clzy3355@outlook.com>
2025-08-07 10:15:40 -0400

    Create servo.c

---
+++ servo.c
@@ -0,0 +1,12 @@
+#include <avr/io.h>
+
+
+void servo_init() {
+//OC1A - PB1: set pin 9 to output
+    DDRB |= 1 << 1;
+
+    TCCR1A |= (0b10 << COM1A0)| (0b10 << WGM10);
+    TCCR1B |= (0b11 << WGM12) | (0b010 << CS10);
+
+    ICR1 = 40000;
+}
