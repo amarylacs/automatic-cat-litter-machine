20fe1d852b702b538849a6b33135a8d3cabcbab0
chenliziyan <clzy3355@outlook.com>
2025-08-12 11:58:11 -0400

    Update Ultrasonic.c, and it works!!!!!!

--- Ultrasonic.c
+++ Ultrasonic.c
@@ -2,13 +2,10 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdbool.h>
-#include "digital.h"
 
 
 #define ECHO_PIN     PB0  // Digital pin 8
 #define TRIGGER_PIN  PD4  // Digital pin 4
-#define LED_PIN      PD3  // Digital pin 3
-#define LED2_PIN     PD5  // Digital pin 5
 
 #define PULSE_TIMEOUT 60000  // Timeout in timer1 ticks (~30ms at 0.5us per tick)
 #define SOUND_SPEED   343.0  // Speed of sound in m/s
@@ -18,7 +15,7 @@ volatile uint16_t pulse_time = 0;
 volatile bool pulse_ready = false;
 volatile uint8_t prev_pinb = 0;
 
-void timer1_init() {
+static void timer1_init() {
     TCCR1A = 0;
     TCCR1B = (1 << CS11);  // Prescaler = 8, 1 tick = 0.5us @16MHz
     TCNT1 = 0;
@@ -41,18 +38,15 @@ ISR(PCINT0_vect) {
             pulse_time = TCNT1 - pulse_start;
             pulse_ready = true;
 
-            PORTD ^= (1 << LED2_PIN);  // Toggle debug LED
         }
     }
 
     prev_pinb = current;
 }
 
-void pin_setup() {
+static void pin_setup() {
     // Trigger pin as output
     DDRD |= (1 << TRIGGER_PIN);
-    // LED pins as output
-    DDRD |= (1 << LED_PIN) | (1 << LED2_PIN);
     // Echo pin as input with pull-up
     DDRB &= ~(1 << ECHO_PIN);
     PORTB |= (1 << ECHO_PIN);
@@ -65,42 +59,33 @@ void pin_setup() {
     PCMSK0 |= (1 << PCINT0);      // Enable pin change on PB0
 }
 
-void send_ultrasonic_pulse() {
+static void send_ultrasonic_pulse() {
     PORTD |= (1 << TRIGGER_PIN);
     _delay_us(10);
     PORTD &= ~(1 << TRIGGER_PIN);
 }
 
-int main(void) {
-    cli();              // Disable global interrupts during setup
+float ultrasonic_distance(void) {
+    cli();
     pin_setup();
     timer1_init();
-    sei();              // Enable global interrupts
+    sei();
 
-    while (1) {
-        pulse_ready = false;
-        send_ultrasonic_pulse();
+    pulse_ready = false;
+    send_ultrasonic_pulse();
 
-        uint16_t start = TCNT1;
-        while (!pulse_ready && ((uint16_t)(TCNT1 - start) < PULSE_TIMEOUT)) {
-            // Wait with timeout (~30ms)
-        }
-
-        float distance_m = -1.0;
-
-        if (pulse_ready) {
-            // Convert ticks to time (0.5us per tick)
-            float time_us = pulse_time * 0.5;
-            distance_m = (time_us / 1000000.0) * (SOUND_SPEED / 2.0);
-        }
-
-        // Display logic
-        if (distance_m > 0 && distance_m < 0.5) {
-            PORTD |= (1 << LED_PIN);   // Turn on LED
-        } else {
-            PORTD &= ~(1 << LED_PIN);  // Turn off LED
-        }
-
-        _delay_ms(200);  // Wait before next measurement
+    uint16_t start = TCNT1;
+    while (!pulse_ready && ((uint16_t)(TCNT1 - start) < PULSE_TIMEOUT)) {
+        // Wait with timeout (~30ms)
     }
+
+    // if (!pulse_ready) {
+    //     return -1.0f;
+    // }
+
+    float distance_m = -1.0;
+    // Convert ticks to time (0.5us per tick)
+    float time_us = pulse_time * 0.5;
+    distance_m = (time_us / 1000000.0) * (SOUND_SPEED / 2.0);
+    return distance_m;
 }
