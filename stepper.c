7d5d2dd2239e83741284b79e8e8569fb356b9d2d
chenliziyan <clzy3355@outlook.com>
2025-08-13 09:42:51 -0400

    Update stepper.c, stepper can stop when ultrasonic interrupt

--- stepper.c
+++ stepper.c
@@ -1,3 +1,5 @@
+#include "stepper.h"
+#include "digital.h"
 #include <util/delay.h>
 #include <avr/io.h>
 #include <stdbool.h>
@@ -18,17 +20,28 @@ void stepper_direction(bool direction) {
 }
 
 static uint16_t map(float angle) {
-    float result = (angle / 360.0) * 200;
+    float result = (angle / 360.0) * STEPS_PER_REV;
     return (uint16_t)result;    
 }
 
-void stepper_set(float angle) {
-    uint16_t step = map(angle);
+static void stepper_move_one_step(void) {
+    PORTD |= (1 << PD3);
+    _delay_us(2000);
+    PORTD &= ~(1 << PD3);
+    _delay_us(2000);
+}
 
-    for (uint8_t i = 0; i < step; i++) {
-        PORTD |= (1 << PD3);
-        _delay_us(1000);
-        PORTD &= ~(1 << PD3);
-        _delay_us(1000);
+void stepper_set(float angle) {
+    uint16_t target_steps = map(angle);
+    float distance = ultrasonic_distance();
+    
+    for (uint16_t i = 0; i < target_steps; i++) {
+        while(distance > 0 && distance < 0.3) {
+            distance = ultrasonic_distance();
+            digitalWrite(13, 1);
+            _delay_ms(10);
+        }
+        stepper_move_one_step();
+        digitalWrite(13,0);
     }
 }
